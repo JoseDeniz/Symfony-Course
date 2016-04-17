868782cc948debe4c234f2a4a2216d5b75e9cce0
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-17 21:03:44 +0100

    Create the Route /login and the login form

--- src/PlanetExpress/UserBundle/Controller/SecurityController.php
+++ src/PlanetExpress/UserBundle/Controller/SecurityController.php
@@ -2,9 +2,38 @@
 
 namespace PlanetExpress\UserBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SecurityController extends Controller
 {
-    
+    /**
+     * @Route("/login", name="login_form")
+     * @Template("@User/security/login.html.twig")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ];
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function logoutAction()
+    {
+        // this is never executed, Symfony intercepts requests to this
+        //return new JsonResponse(['error' => 'you fail!']);
+    }
 }
