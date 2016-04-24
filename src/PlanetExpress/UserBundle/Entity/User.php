bb918e577426cf231c9c721d716f5fdb07a4adf9
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-24 22:18:37 +0100

    Implement serializable interface in User

--- src/PlanetExpress/UserBundle/Entity/User.php
+++ src/PlanetExpress/UserBundle/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\AdvancedUserInterface;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="PlanetExpress\UserBundle\Repository\UserRepository")
  */
-class User implements AdvancedUserInterface
+class User implements AdvancedUserInterface, \Serializable
 {
     /**
      * @var int
@@ -64,7 +64,7 @@ class User implements AdvancedUserInterface
     {
         $this->isActive = true;
     }
-    
+
     /**
      * @param array $roles
      */
@@ -266,6 +266,39 @@ class User implements AdvancedUserInterface
     public function setEmail($email)
     {
         $this->email = $email;
+    }
+
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password
+        ]);
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->password
+            ) = unserialize($serialized);
     }
 }
 
