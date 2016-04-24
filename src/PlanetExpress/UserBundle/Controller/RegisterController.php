6e7cf623aa4ebbba6f7f1b7ca6326b64fdbc56a0
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-04-25 00:05:00 +0100

    Add RegisterController and registerAction

---
+++ src/PlanetExpress/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PlanetExpress\UserBundle\Controller;
+
+use PlanetExpress\UserBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     * @Template("UserBundle:Register:register.html.twig")
+     * @param Request $request
+     * @return array
+     */
+    public function registerAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, ['type' => PasswordType::class])
+            ->add('Register!', SubmitType::class, ['label' => 'Register'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $user = new User($data['username'], $data['email']);
+            $user->setPassword($this->encodePassword($user, $data['password']));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('event_index'));
+        }
+
+        return ['form' => $form->createView()];
+    }
+
+    /**
+     * @param User $user
+     * @param $plainPassword
+     * @return string
+     */
+    private function encodePassword($user, $plainPassword)
+    {
+        $encoder = $this->get('security.encoder_factory')->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+}
